5c40df85bd7be58ef4f5de7f7d1b11ccd7c795aa
Jonathan Dominguez <jonathandominguez1616@gmail.com>
2025-09-07 19:39:14 -0500

    Added hello.py

---
+++ hello.py
@@ -0,0 +1,6 @@
+#print a introduction to this project
+print("Hello, Github + VSCode!")
+#Ask for users name
+name = input("Please enter your name:")
+#print the users input with a personalized greeting
+print('Hello,', name) 
